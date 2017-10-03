3a34c786950ac06fabf6f3e1596c8bab568d9aaa
Martin <martin_thomas@alumni.baylor.edu>
2017-10-02 19:57:38 -0500

    removed jscript, kept the tree though

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,12 +14,3 @@
 //= require turbolinks
 //= require_tree 
 //= jquery
-function main() {
-	$('.answers-box').hide();
-  $('.qaa-box').on('click', function() {
-    $(this).next().slideToggle(400);
-    console.log('test');
-  });
-}
-
-$(document).on('turbolinks:load', function(main(););
