5e8122aec2dfa7cb9514be69140ffd532bb628ad
Martin <martin_thomas@alumni.baylor.edu>
2017-10-04 14:54:03 -0500

    javascript fixed with turbolinks

--- app/assets/javascripts/harveyfaq.js
+++ app/assets/javascripts/harveyfaq.js
@@ -1,7 +1,6 @@
-function main() {
-	$('.answers-box').hide();
-  $('.qaa-box').on('click', function() {
-    $(this).next().slideToggle(400);
-  });
-}
-document.addEventListener("turbolinks:load", function() { alert("ok!") });
+
+document.addEventListener("turbolinks:load", function() { 
+  $('.qaa-title').on('click', function() {
+    $(this).next().slideToggle(400);})
+    $(".answers-box").hide(); 
+});
