84e2dd626d847b1fb828f5ccfe2bdd2f72d04515
Martin <martin_thomas@alumni.baylor.edu>
2017-10-04 12:05:28 -0500

    playing with more javascript

--- app/assets/javascripts/harveyfaq.js
+++ app/assets/javascripts/harveyfaq.js
@@ -2,8 +2,6 @@ function main() {
 	$('.answers-box').hide();
   $('.qaa-box').on('click', function() {
     $(this).next().slideToggle(400);
-    console.log('test');
   });
 }
-
-$(document).ready(main);
+document.addEventListener("turbolinks:load", function() { alert("ok!") });
