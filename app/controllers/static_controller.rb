d7b6c0fca18c6768c608c66e714140dc38d4a9c7
Martin <martin_thomas@alumni.baylor.edu>
2018-01-30 11:55:01 -0600

    reinitialized static controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,28 +1,30 @@
 class StaticController < ApplicationController
-  
-  def home
-  end
-
-  def maintenance
-  end
-
-  def permission_error
-    respond_to do |format|
-      format.js
-      format.html
-    end 
-  end
-
-  def contact
-  end
-
-  def harvey_faq
-  end
-  
-  def doesnotexist
-  end
-
-  def help
-  end 
-  
+    def home
+        respond_to do |format|
+          format.html
+          format.js 
+        end 
+      end
+    
+      def maintenance
+      end
+    
+      def permission_error
+        respond_to do |format|
+          format.js
+          format.html
+        end 
+      end
+    
+      def contact
+      end
+    
+      def harvey_faq
+      end
+      
+      def doesnotexist
+      end
+    
+      def help
+      end 
 end
--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,24 +1,7 @@
 require 'test_helper'
 
 class StaticControllerTest < ActionDispatch::IntegrationTest
-  test "should get Home" do
-    get @home
-    assert_response :success
-  end
-
-  test "should get Maintenance" do
-    get @maintenance
-    assert_response :success
-  end
-
-  test "should get PermissionError" do
-    get @permissionError
-    assert_response :success
-  end
-
-  test "should get Contact" do
-    get @contact
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
