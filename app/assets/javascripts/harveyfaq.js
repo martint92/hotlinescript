53728da7c253bfc4feaf2875dc37abb3e9ccb5b0
Martin <martin_thomas@alumni.baylor.edu>
2018-02-03 12:16:24 -0600

    Fixed double firing JS

--- app/assets/javascripts/harveyfaq.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("turbolinks:load", function() { 
-    $('.qaa-title').on('click', function() {
-      $(this).next().slideToggle(400);
-      $(this).find('img').toggle();
-    })
-      $(".answers-box").hide(); 
-  });
