b278f54696990f060805944645ccf40fddb74a79
Martin <martin_thomas@alumni.baylor.edu>
2018-02-06 10:12:37 -0600

    fixed javascript/turbolinks issues

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,10 @@
 //= require cocoon
 //= require_tree .
 
+document.addEventListener("turbolinks:load", function() {
+  $('.qaa-title').on('click', function() {
+    $(this).next().slideToggle(400);
+    $(this).find('img').toggle();
+    })
+	$(".answers-box").hide(); 
+})
