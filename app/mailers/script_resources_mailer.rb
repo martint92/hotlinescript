f60738a6da3f828782b57ed88600497e4abc57dc
Martin <martin_thomas@alumni.baylor.edu>
2018-02-06 05:21:55 -0600

    minor tweek to config

--- app/mailers/script_resources_mailer.rb
+++ app/mailers/script_resources_mailer.rb
@@ -1,9 +1,17 @@
 class ScriptResourcesMailer < ApplicationMailer
-    default from: "hotline@houstonimmigration.org"
+    # default from: "hotline@houstonimmigration.org"
+    default from: "immigrationhotlineTX@gmail.com"
 
     # http://localhost:3000/rails/mailers/script_resources_mailer/test_email_preview.html
     def test_email(guest)
         @guest = guest 
-        mail(to: @guest, subject: 'Test Email')
+        # mail(to: @guest, subject: 'Test Email')
+        mail(to: "martin_thomas@alumni.baylor.edu", subject: 'Test Email')
     end 
+
+    def sample_email(user)
+        recipients  user.email 
+        from        "Immigration Hotline <immigrationhotlineTX@gmail.com>"
+        subject     "Testing Sample Email"
+        sent_to     Time.now 
 end
